1c07e7c4c1a81f89c49d881549b5127bd8d52a3b
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-03-31 06:12:57 +0700

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -44,6 +44,6 @@ async def remt(event):
   
 @Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
-    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @TeamDrone"
+    text = "Kirim link post channel yang di proteksi ke bot ini, Pastikan anda sudah join terlebih dahulu channel pemilik konten (post channel) .\n\n**SUPPORT:** @nakama_asl"
     await start_srb(event, text)
     
